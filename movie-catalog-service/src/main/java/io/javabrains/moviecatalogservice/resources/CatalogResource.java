a13d480e3f974e5765847f6738f2909778eebc2d
Java Brains <javabrains@Koushiks-MacBook-Pro.local>
2019-01-25 22:08:49 -0800

    06. Hard coded ratings in catalog service

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/CatalogResource.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/CatalogResource.java
@@ -1,12 +1,15 @@
 package io.javabrains.moviecatalogservice.resources;
 
 import io.javabrains.moviecatalogservice.models.CatalogItem;
+import io.javabrains.moviecatalogservice.models.Rating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
@@ -14,6 +17,14 @@ public class CatalogResource {
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-        return Collections.singletonList(new CatalogItem("Test", "Test Desc", 4));
+        List<Rating> ratingsList = Arrays.asList(
+                new Rating("1234", 3),
+                new Rating("5678", 4)
+        );
+
+        return ratingsList.stream()
+                .map(rating -> new CatalogItem("Name", "Desc", rating.getRating()))
+                .collect(Collectors.toList());
+
     }
 }
