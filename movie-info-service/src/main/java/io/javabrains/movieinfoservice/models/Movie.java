c38f986b9b56d91dd751980bad2675547173c1bb
Java Brains <javabrains@Koushiks-MacBook-Pro.local>
2019-01-25 20:45:43 -0800

    03. Added Movie Info API

---
+++ movie-info-service/src/main/java/io/javabrains/movieinfoservice/models/Movie.java
@@ -0,0 +1,27 @@
+package io.javabrains.movieinfoservice.models;
+
+public class Movie {
+    private String movieId;
+    private String name;
+
+    public Movie(String movieId, String name) {
+        this.movieId = movieId;
+        this.name = name;
+    }
+
+    public String getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(String movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
