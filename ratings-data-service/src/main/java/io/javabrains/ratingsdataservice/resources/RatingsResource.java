568433f59a27b3f9e5139e6852c322d1f3467acb
Java Brains <javabrains@Koushiks-MacBook-Pro.local>
2019-01-25 21:39:56 -0800

    05. Added Ratings Data Service API

---
+++ ratings-data-service/src/main/java/io/javabrains/ratingsdataservice/resources/RatingsResource.java
@@ -0,0 +1,16 @@
+package io.javabrains.ratingsdataservice.resources;
+
+import io.javabrains.ratingsdataservice.model.Rating;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingsResource {
+
+    @RequestMapping("/{movieId}")
+    public Rating getRating(@PathVariable("movieId") String movieId) {
+        return new Rating(movieId, 4);
+    }
+}
