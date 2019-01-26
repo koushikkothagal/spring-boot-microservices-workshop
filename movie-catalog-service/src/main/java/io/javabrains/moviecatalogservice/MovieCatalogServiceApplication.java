6a1fafaaded912712f9bed2ee0f23ab278694d3f
Java Brains <javabrains@Koushiks-MacBook-Pro.local>
2019-01-26 11:26:31 -0800

    10. Converted services to Eureka clients

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -2,10 +2,12 @@ package io.javabrains.moviecatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MovieCatalogServiceApplication {
 
 	public static void main(String[] args) {
--- movie-info-service/src/main/java/io/javabrains/movieinfoservice/MovieInfoServiceApplication.java
+++ movie-info-service/src/main/java/io/javabrains/movieinfoservice/MovieInfoServiceApplication.java
@@ -2,8 +2,10 @@ package io.javabrains.movieinfoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MovieInfoServiceApplication {
 
 	public static void main(String[] args) {
