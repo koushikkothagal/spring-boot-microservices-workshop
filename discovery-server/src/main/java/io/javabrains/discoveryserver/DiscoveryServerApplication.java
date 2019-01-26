4f844d32763837005020f5325069c2927e310b6a
Java Brains <javabrains@Koushiks-MacBook-Pro.local>
2019-01-26 10:52:16 -0800

    09. Added Eureka server

---
+++ discovery-server/src/main/java/io/javabrains/discoveryserver/DiscoveryServerApplication.java
@@ -0,0 +1,16 @@
+package io.javabrains.discoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServerApplication.class, args);
+	}
+
+}
+
