9438af238c1ab75b385bb6a9395171fe078b274b
nakulGoyal19 <nakul19goyal@gmail.com>
2020-04-14 21:51:09 +0530

    config-server application

--- config/src/main/java/test/sever/config/ConfigApplication.java
+++ config/src/main/java/test/sever/config/ConfigApplication.java
@@ -2,8 +2,10 @@ package test.sever.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigApplication {
 
 	public static void main(String[] args) {
