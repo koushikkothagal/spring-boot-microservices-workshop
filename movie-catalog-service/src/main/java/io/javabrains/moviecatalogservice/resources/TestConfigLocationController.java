0c5ef7ce3d49b77858b652b19c4bcce735f2962f
nakulGoyal19 <nakul19goyal@gmail.com>
2020-04-14 23:42:53 +0530

    Spring-cloud-config-client implementation on movie-catalog-service and some updates

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/TestConfigLocationController.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/TestConfigLocationController.java
@@ -2,10 +2,12 @@ package io.javabrains.moviecatalogservice.resources;
 
 import org.bouncycastle.asn1.pkcs.SafeBag;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class TestConfigLocationController {
     
     @Value("${test.config.location}")
