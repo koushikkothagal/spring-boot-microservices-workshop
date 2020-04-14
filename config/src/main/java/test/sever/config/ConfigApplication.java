7fde53cda2866ddd824f20e35e1b8dfb84ead79a
nakulGoyal19 <nakul19goyal@gmail.com>
2020-04-14 21:49:53 +0530

    config-file repo added

---
+++ config/src/main/java/test/sever/config/ConfigApplication.java
@@ -0,0 +1,13 @@
+package test.sever.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigApplication.class, args);
+	}
+
+}
---
+++ config/src/test/java/test/sever/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package test.sever.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
