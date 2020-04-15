c9fb2e1a57aeb9bac1327ba229e6f79302b4d1ef
nakulGoyal19 <nakul19goyal@gmail.com>
2020-04-15 18:09:13 +0530

    Applied Logging for configLocation controller with logback.xml file

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/TestConfigLocationController.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/TestConfigLocationController.java
@@ -1,6 +1,8 @@
 package io.javabrains.moviecatalogservice.resources;
 
 import org.bouncycastle.asn1.pkcs.SafeBag;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,11 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RefreshScope
 public class TestConfigLocationController {
     
+    private static final Logger LOG = LoggerFactory.getLogger(TestConfigLocationController.class);
+    
     @Value("${test.config.location}")
     private String testConfigLocation;
     
     @GetMapping("/configLocation")
     public String getTestConfigLocation(){
+        LOG.info("Testing the location of config file for movie-catalog-service");
         return testConfigLocation;
     }
     
